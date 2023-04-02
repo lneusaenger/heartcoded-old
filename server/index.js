fe649b9a4d90af59acdf1e46f140a403c6db1f6f
Lily Neusaenger <lily.neusaenger@duke.edu>
2023-04-02 11:06:01 -0400

    not sure

--- server/index.js
+++ server/index.js
@@ -10,17 +10,16 @@ const morgan = require("morgan");
 const app = express();
 
 mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => console.log("DB connecte"))
-    .catch((err) => console.log("DB Connection error: ", err));
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Connection error: ", err));
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
 
 app.use("/api", authRoutes);
 
-app.listen(8000, () => console.log("Server running on port 3000"));
-
+app.listen(8000, () => console.log("Server running on port 8000"));
